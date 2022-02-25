2d980166b7018e42fdec42967622c640369c534b
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-25 19:03:12 +0300

    TokenDto class is created

--- TodoApp.Core/DTOs/TokenDto.cs
+++ TodoApp.Core/DTOs/TokenDto.cs
@@ -8,5 +8,9 @@ namespace TodoApp.Core.DTOs
 {
     public class TokenDto
     {
+        public string AccessToken { get; set; }
+        public DateTime AccessTokenExpiration { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiration { get; set; }
     }
 }
