8e1e71fc18e86adc1191ab8af1149c2a9667961b
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-25 18:18:42 +0300

    IGenericRepository class added

--- TodoApp.Core/Model/Work.cs
+++ TodoApp.Core/Model/Work.cs
@@ -8,8 +8,8 @@ namespace TodoApp.Core.Model
 {
     public class Work:BaseEntity
     {
+        public string Name { get; set; }
         public string Definition { get; set; }
-        public string Description { get; set; }
         public bool IsCompleted { get; set; }
     }
 }
