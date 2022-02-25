08afc2cbecccdd0c16fd8688c8a701a6019749de
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-25 19:30:20 +0300

    ClientLoginDto

--- TodoApp.Core/Configuration/ITokenService.cs
+++ TodoApp.Core/Configuration/ITokenService.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TodoApp.Core.DTOs;
+using TodoApp.Core.Model;
 
 namespace TodoApp.Core.Configuration
 {
     public interface ITokenService
     {
-        TokenDto CreateToken(UserApp userApp);
+        TokenDto CreateToken(User user);
         ClientTokenDto CreateTokenByClient(Client client);
     }
 }
--- TodoApp.Core/DTOs/ClientLoginDto.cs
+++ TodoApp.Core/DTOs/ClientLoginDto.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace TodoApp.Core.DTOs
 {
-    public class LoginDto
+    public class ClientLoginDto
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
     }
 }
