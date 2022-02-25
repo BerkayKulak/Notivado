f7460a07a3ad49a220ad8f7de52168fdca5e746d
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-25 21:30:18 +0300

    AppDbContext

---
+++ TodoApp.Repository/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Core.Model;
+namespace TodoApp.Repository
+{
+    public class AppDbContext : IdentityDbContext<User, IdentityRole, string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Work> Products { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
