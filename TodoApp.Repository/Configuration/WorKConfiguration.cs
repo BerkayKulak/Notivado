d75d94adcd50380e2b385c85df12e8470419db67
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-25 21:40:25 +0300

    WorKConfiguration

--- TodoApp.Repository/Configuration/WorKConfiguration.cs
+++ TodoApp.Repository/Configuration/WorKConfiguration.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TodoApp.Core.Model;
 
 namespace TodoApp.Repository.Configuration
 {
-    internal class WorKConfiguration
+    public class WorKConfiguration : IEntityTypeConfiguration<Work>
     {
+        public void Configure(EntityTypeBuilder<Work> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Definition).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.IsCompleted).IsRequired();
+        }
     }
 }
