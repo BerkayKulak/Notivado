3836bfc5cc86438d6629a859c9e0b69ffa3f53e4
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-25 22:47:40 +0300

    TokenService

--- TodoApp.Service/Services/TokenService.cs
+++ TodoApp.Service/Services/TokenService.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using TodoApp.Core.Configuration;
+using TodoApp.Core.DTOs;
+using TodoApp.Core.Model;
 
 namespace TodoApp.Service.Services
 {
-    public class TokenService
+    public class TokenService : ITokenService
     {
+        private readonly UserManager<User> _userManager;
+        private readonly CustomTokenOptions _tokenOptions;
+
+        public TokenService(UserManager<User> userManager, IOptions<CustomTokenOptions> tokenOptions)
+        {
+            _userManager = userManager;
+            _tokenOptions = tokenOptions.Value;
+        }
+
+        private string CreateRefreshToken()
+        {
+            var numberByte = new Byte[32];
+
+            using var rnd = RandomNumberGenerator.Create();
+
+            rnd.GetBytes(numberByte);
+
+            return Convert.ToBase64String(numberByte);
+        }
+
+        public TokenDto CreateToken(User userApp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
