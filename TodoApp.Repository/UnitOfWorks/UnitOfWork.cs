fad27877a04b940c4d86c703ece7088b43770518
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-25 22:08:41 +0300

    UnitOfWork

---
+++ TodoApp.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Core.UnitOfWorks;
+
+namespace TodoApp.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
