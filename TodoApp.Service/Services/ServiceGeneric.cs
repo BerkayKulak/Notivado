f2fd7b7aef4c960456bc77f8b250d150bcc03c89
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-25 22:26:48 +0300

    GenericService class’sının oluşturulması

---
+++ TodoApp.Service/Services/ServiceGeneric.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Core.DTOs;
+using TodoApp.Core.Repositories;
+using TodoApp.Core.Services;
+using TodoApp.Core.UnitOfWorks;
+using TodoApp.Service.Mapping;
+
+namespace TodoApp.Service.Services
+{
+    public class ServiceGeneric<TEntity, TDto> : IServiceGeneric<TEntity, TDto> where TEntity : class where TDto : class
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        private readonly IGenericRepository<TEntity> _genericRepository;
+
+        public ServiceGeneric(IUnitOfWork unitOfWork, IGenericRepository<TEntity> genericRepository)
+        {
+            _unitOfWork = unitOfWork;
+
+            _genericRepository = genericRepository;
+        }
+
+        public async Task<Response<TDto>> GetByIdAsync(int id)
+        {
+            var product = await _genericRepository.GetByIdAsync(id);
+
+            if (product == null)
+            {
+                return Response<TDto>.Fail("Id not found", 404, true);
+            }
+
+            return Response<TDto>.Success(ObjectMapper.Mapper.Map<TDto>(product), 200);
+        }
+
+        public async Task<Response<IEnumerable<TDto>>> GetAllAsync()
+        {
+            var products = ObjectMapper.Mapper.Map<List<TDto>>(await _genericRepository.GetAllAsync());
+
+            return Response<IEnumerable<TDto>>.Success(products, 200);
+        }
+
+        public async Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate)
+        {
+            var list = _genericRepository.Where(predicate);
+
+            return Response<IEnumerable<TDto>>.Success(ObjectMapper.Mapper.Map<IEnumerable<TDto>>(await list.ToListAsync()), 200);
+
+
+        }
+
+        public async Task<Response<TDto>> AddAsync(TDto entity)
+        {
+            var newEntity = ObjectMapper.Mapper.Map<TEntity>(entity);
+
+            await _genericRepository.AddAsync(newEntity);
+
+            await _unitOfWork.CommitAsync();
+
+            var newDto = ObjectMapper.Mapper.Map<TDto>(newEntity);
+
+            return Response<TDto>.Success(newDto, 200);
+
+        }
+
+        public async Task<Response<NoDataDto>> Remove(int id)
+        {
+            var isExistEntity = await _genericRepository.GetByIdAsync(id);
+
+            if (isExistEntity == null)
+            {
+                return Response<NoDataDto>.Fail("Id not found", 404, true);
+            }
+
+            _genericRepository.Remove(isExistEntity);
+
+            await _unitOfWork.CommitAsync();
+
+            //204 durum kodu = No content => bodysinde hiç bir data olmayacak.
+            return Response<NoDataDto>.Success(204);
+        }
+
+        public async Task<Response<NoDataDto>> Update(TDto entity, int id)
+        {
+            var isExistEntity = await _genericRepository.GetByIdAsync(id);
+
+            if (isExistEntity == null)
+            {
+                return Response<NoDataDto>.Fail("Id not found", 404, true);
+            }
+
+            var updatedEntity = ObjectMapper.Mapper.Map<TEntity>(entity);
+
+            _genericRepository.Update(updatedEntity);
+
+            await _unitOfWork.CommitAsync();
+
+            return Response<NoDataDto>.Success(204);
+
+
+        }
+    }
+}
