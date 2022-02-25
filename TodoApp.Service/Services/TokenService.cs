7515cdfe1be2181f0c722d06320c3370f03d6138
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-25 22:51:11 +0300

    TokenService

--- TodoApp.Service/Services/TokenService.cs
+++ TodoApp.Service/Services/TokenService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +27,8 @@ namespace TodoApp.Service.Services
 
         private string CreateRefreshToken()
         {
+
+
             var numberByte = new Byte[32];
 
             using var rnd = RandomNumberGenerator.Create();
@@ -32,6 +36,24 @@ namespace TodoApp.Service.Services
             rnd.GetBytes(numberByte);
 
             return Convert.ToBase64String(numberByte);
+        }
+
+        private IEnumerable<Claim> GetClaim(User userApp, List<String> audiences)
+        {
+            var userList = new List<Claim>()
+            {
+                // kullanıcının kimliği oludğundan dolayı ID ye karşılık geliyor.
+                new Claim(ClaimTypes.NameIdentifier,userApp.Id),
+                new Claim(JwtRegisteredClaimNames.Email,userApp.Email),
+                new Claim(ClaimTypes.Role,userApp.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            // Audience bakacak gerçekten kendisinie istek yapılmaya uygun mu kontrol edecek, eğer uygun değilse tokeni geri çevirecek.
+            userList.AddRange(audiences.Select(x => new Claim(JwtRegisteredClaimNames.Aud, x)));
+
+            return userList;
+
         }
 
         public TokenDto CreateToken(User userApp)
