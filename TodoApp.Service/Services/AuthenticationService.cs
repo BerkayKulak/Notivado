1d0c92d7cda7585aa7b4bde081a6fb880ed5fbc3
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-25 22:59:25 +0300

    AuthenticationService - 1

---
+++ TodoApp.Service/Services/AuthenticationService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using TodoApp.Core.Configuration;
+using TodoApp.Core.DTOs;
+using TodoApp.Core.Model;
+using TodoApp.Core.Repositories;
+using TodoApp.Core.Services;
+using TodoApp.Core.UnitOfWorks;
+
+namespace TodoApp.Service.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly List<Client> _clients;
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<User> _userManager;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericRepository<UserRefreshToken> _userRefreshTokenService;
+
+        public AuthenticationService(IOptions<List<Client>> optionsClient, ITokenService tokenService, UserManager<User> userManager, IUnitOfWork unitOfWork, IGenericRepository<UserRefreshToken> userRefreshTokenService)
+        {
+            _clients = optionsClient.Value;
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _unitOfWork = unitOfWork;
+            _userRefreshTokenService = userRefreshTokenService;
+        }
+
+
+        public Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
