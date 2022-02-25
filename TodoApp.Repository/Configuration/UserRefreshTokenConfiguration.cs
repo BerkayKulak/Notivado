30c0c132a3a5647f867a39c205d4742d51d4ead7
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-25 21:47:39 +0300

    UserRefreshTokenConfiguration is added

--- TodoApp.Repository/Configuration/UserRefreshTokenConfiguration.cs
+++ TodoApp.Repository/Configuration/UserRefreshTokenConfiguration.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TodoApp.Core.Model;
 
 namespace TodoApp.Repository.Configuration
 {
-    internal class UserRefreshTokenConfiguration
+    public class UserRefreshTokenConfiguration : IEntityTypeConfiguration<UserRefreshToken>
     {
+        public void Configure(EntityTypeBuilder<UserRefreshToken> builder)
+        {
+            builder.HasKey(x => x.UserId);
+            builder.Property(x => x.Code).IsRequired().HasMaxLength(200);
+        }
     }
+}
 }
